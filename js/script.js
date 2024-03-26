e7103e63776db612b19b6dfcf115c927f3637dd5
simone-danzeri <s.danzeri@icloud.com>
2024-03-26 13:25:19 +0100

    feat: completo

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            message: 'Il potere di Vue Js',
+        };
+    },
+    methods: {
+
+    }
+}).mount('#app');
